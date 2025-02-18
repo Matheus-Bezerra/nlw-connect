8ffb9fb729afc45087ded8ce93556f6446815343
Matheus-BezerraaVanguard <matheus.bezerra@sistemavangurd.com.br>
2025-02-17 21:55:28 -0300

    initialize Next.js project with Tailwind CSS and create basic components

---
+++ src/components/button.tsx
@@ -0,0 +1,10 @@
+import React, { ComponentProps, ReactNode } from 'react'
+
+interface ButtonProps extends ComponentProps<'button'> {}
+
+export const Button = (props: ButtonProps) => {
+    return (
+        <button {...props} className='px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full flex items-center justify-between cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900'>
+        </button>
+    )
+}
